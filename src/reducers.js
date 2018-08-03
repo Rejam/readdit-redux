76e2794d941cc0dfe95f6b6e51c82e41ccbb5599
Ryan James <rejames@gmail.com>
2018-08-03 21:42:04 +0100

    reducers updated for all actions

--- src/reducers.js
+++ src/reducers.js
@@ -1,21 +1,49 @@
 import { combineReducers } from "../node_modules/redux";
+import { SELECT_SUB, INVALIDATE_SUB, REQUEST_POSTS, RECEIVE_POSTS } from "./actions";
 
-const subReducer = (state, action) => {
+const selectedSub = (state = 'popular', action) => {
   switch(action.type) {
+    case SELECT_SUB:
+      return action.sub
     default:
       return state
   }
 }
 
-const postsReducer = (state, action) => {
+const postsBySub = (state = {}, action) => {
   switch(action.type) {
+    case INVALIDATE_SUB:
+      return {
+        ...state,
+        [action.sub]: {
+          didInvalidate: true
+        }
+      }
+    case REQUEST_POSTS:
+      return {
+        ...state, 
+        [action.sub]: {
+          isFetching: true,
+          didInvalidate: false
+        }
+      }
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        [action.sub]: {
+          isFetching: false,
+          didInvalidate: false,
+          lastUpdated: action.lastUpdated,
+          items: action.posts
+        }
+      }
     default:
       return state
   }
 }
 
 const rootReducer = combineReducers({
-  selectedSub: subReducer,
-  postsBySub: postsReducer
+  selectedSub,
+  postsBySub
 })
 export default rootReducer
